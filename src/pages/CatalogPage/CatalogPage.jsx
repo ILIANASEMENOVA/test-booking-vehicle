df95ca8db3c0dd5cddf13caf6dd3bddb57f5bc00
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2024-03-22 23:03:43 +0200

    feat: add fetchAdverts in CatalogPage

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectAdverts } from "../../redux/adverts/selectors";
+import { fetchAdverts } from "../../redux/adverts/operations";
+
 const CatalogPage = () => {
+  const dispatch = useDispatch();
+  const data = useSelector(selectAdverts);
+
+  useEffect(() => {
+    dispatch(fetchAdverts());
+  }, [dispatch]);
+
+  console.log(data);
+
   return <div>CatalogPage</div>;
 };
 
