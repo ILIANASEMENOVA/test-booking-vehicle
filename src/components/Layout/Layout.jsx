231ad0581d67cde578bfc96decf28f79c78f268a
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2024-03-22 21:17:43 +0200

    frat: add basic navigation

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,14 @@
 import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
     <div>
+      <nav style={{ display: "flex", justifyContent: "center", gap: 20 }}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/catalog">Catalog</NavLink>
+        <NavLink to="/favorites">Favorites</NavLink>
+      </nav>
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
